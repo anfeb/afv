370106da493946e868f2d30b357377800bf6774c
anfeb <88554978+anfeb@users.noreply.github.com>
2021-08-28 14:53:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts = require('cfonts')
-CFonts.say('Aguz Familia', {
+CFonts.say('anfebn', {
   colors: ['#90A0A0'],
   font: 'block',
   align: 'center',
